26119aee327fd014c45fff14880db693ad0dd14e
chiru26 <90021190+chiru26@users.noreply.github.com>
2022-04-02 21:56:22 +0530

    Add files via upload

---
+++ 3_Implementation/Inc/welcome.h
@@ -0,0 +1,50 @@
+void welcome()
+{
+                system("color f1");
+                int i,y;
+                locate(26,5);
+                printf(" _ _ _ _ _ _ _ _ _ _  _ _ _ _ ");
+                locate(27,7);
+                  printf("*****************************");
+                  locate(27,9);
+                  printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c"
+                         ,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3);
+            locate(27,11);
+                  printf(" %c%c%c!!! WELCOME !!!%c%c%c",2,2,2,2,2,2);
+                  locate(39,13);
+                  printf("TO");
+                  locate(27,15);
+                  printf(" %c%c%c%c CRICKET SCORE SHEET %c%c%c%c",3,3,3,3,3,3,3,3);
+                  locate(27,17);
+                  printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%"
+                         ,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3);
+                  locate(27,19);
+                  printf("*****************************");
+                  locate(27,21);
+                  printf("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+                  Sleep(250);
+                  locate(27,23);
+                  Sleep(250);
+                  locate(27,25);
+                  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN | FOREGROUND_INTENSITY | FOREGROUND_BLUE );
+                  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN | FOREGROUND_INTENSITY | FOREGROUND_BLUE );
+                  locate(27,29);
+                  printf("\t welcome to cricket score card management");
+
+                  for(y=30;y<=40;y++)
+                  {
+                    {
+
+                      locate(27,y);
+                      Sleep(400);
+                    }
+                  }
+                  printf("\n");
+
+system("color ff");
+            }
+
+
+
+
+
